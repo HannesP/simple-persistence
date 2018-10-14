1437dd3cb4e4df8375b6716040dbec2f3eed6154
Hannes Petri <mail@hannesp.se>
2018-10-14 14:57:51 +0200

    Added id and opening logic to attachment

--- app/Message.ts
+++ app/Message.ts
@@ -1,6 +1,7 @@
 import { EntityState, Entity, addEntity } from "../lib/Entity";
 
 interface AttachmentState extends EntityState {
+  id: string;
   size: number;
   opened: boolean;
   filename: string;
@@ -13,16 +14,25 @@ export interface MessageState extends EntityState {
 }
 
 export class Attachment extends Entity<AttachmentState> {
-  static from(size: number, filename: string) {
+  static from(id: string, size: number, filename: string) {
     return new Attachment({
+      id,
       size,
       filename,
       opened: false
     });
   }
 
+  get id() {
+    return this.state.id;
+  }
+
   get size() {
     return this.state.size;
+  }
+
+  open() {
+    this.state.opened = true;
   }
 }
 
@@ -40,8 +50,22 @@ export class Message extends Entity<MessageState> {
   }
 
   addAttachment(attachment: Attachment) {
-    if (attachment.size < 1024*1024) {
-      addEntity(this.state.attachments, attachment);
+    const ids = this.attachments.map(att => att.id);
+    if (ids.indexOf(attachment.id) !== -1) {
+      return;
+    }
+
+    if (attachment.size >= 1024 * 1024) {
+      return;
+    }
+
+    addEntity(this.state.attachments, attachment);
+  }
+
+  openAttachment(id: string) {
+    const attachment = this.attachments.find(att => att.id === id);
+    if (attachment) {
+      attachment.open();
     }
   }
 }
--- index.ts
+++ index.ts
@@ -6,12 +6,17 @@ async function start() {
   const messageRepository: MessageRepository = new InMemoryMessageRepository();
 
   const message = Message.from("Hannes", "Sofia");
-  message.addAttachment(Attachment.from(23423, "image.png"));
+  message.addAttachment(Attachment.from("att_923478", 23423, "image.png"));
+  message.openAttachment("att_923478");
 
   const id = await messageRepository.persist(message);
   const revived = await messageRepository.fetchById(id);
 
-  console.log(revived);
+  if (revived == null) {
+    console.log("Not found");
+  } else {
+    console.log(revived.attachments);
+  }
 }
 
 start();
